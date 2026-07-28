79b289cbfd54aa647f1171ef3ead2e60d0477c89
Aleksei Gerasev <1999bag@gmail.com>
2026-07-28 18:40:18 +0300

    feat: refactor PostgreSQL connection handling to use pgx and improve error management

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,7 +8,7 @@ import (
 	"syscall"
 
 	core_logger "github.com/Atmosfr/golang-todoapp/internal/core/logger"
-	core_postgres_pool "github.com/Atmosfr/golang-todoapp/internal/core/repository/postgres/pool"
+	core_pgx_pool "github.com/Atmosfr/golang-todoapp/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/Atmosfr/golang-todoapp/internal/core/transport/http/middleware"
 	core_http_server "github.com/Atmosfr/golang-todoapp/internal/core/transport/http/server"
 	users_postgres_repository "github.com/Atmosfr/golang-todoapp/internal/features/users/repository/postgres"
@@ -30,10 +30,11 @@ func main() {
 	defer logger.Close()
 
 	logger.Debug("initializing postgres connection pool")
-	pool, err := core_postgres_pool.NewConnectionPool(
+	pool, err := core_pgx_pool.NewPool(
 		ctx,
-		core_postgres_pool.NewConfigMust(),
+		core_pgx_pool.NewConfigMust(),
 	)
+
 	if err != nil {
 		logger.Fatal("failed to init postgres connection pool", zap.Error(err))
 	}
--- internal/features/users/repository/postgres/patch_user.go
+++ internal/features/users/repository/postgres/patch_user.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/Atmosfr/golang-todoapp/internal/core/domain"
 	core_errors "github.com/Atmosfr/golang-todoapp/internal/core/errors"
-	"github.com/jackc/pgx/v5"
+	core_postgres_pool "github.com/Atmosfr/golang-todoapp/internal/core/repository/postgres/pool"
 )
 
 func (r *UsersRepository) PatchUser(
@@ -46,7 +46,7 @@ func (r *UsersRepository) PatchUser(
 	)
 
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
+		if errors.Is(err, core_postgres_pool.ErrNoRows) {
 			return domain.User{}, fmt.Errorf(
 				"user with id='%d' concurrently accessed: %w",
 				userID,
